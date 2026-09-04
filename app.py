de3b4d21926823074548f3574ea192bf9d885cea
Satyapatcha <satyapuli919@gmail.com>
2026-09-04 18:45:29 +0530

    Refactor app.py to remove complex math and add strings
    Removed square root calculation for complex numbers and added string examples.

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-# Find square root of real or complex numbers
-# Importing the complex math module
-import cmath
+s = """I am Learning
+Python String on GeeksforGeeks"""
+print(s)
 
-num = 1+2j
-
-# To take input from the user
-#num = eval(input('Enter a number: '))
-
-num_sqrt = cmath.sqrt(num)
-print('The square root of {0} is {1:0.3f}+{2:0.3f}j'.format(num ,num_sqrt.real,num_sqrt.imag))
+s = '''I'm a 
+Geek'''
+print(s)
